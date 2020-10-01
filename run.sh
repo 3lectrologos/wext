a0f4d6b2a7cf70c8b4a7749cafcb1e7d56f80a5f
Alkis Gotovos <alkisg@mit.edu>
2020-10-01 16:54:06 -0400

    Update run script

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 echo "WExT k = $1"
+echo "PID = $2"
+PID=$2
+FDATA="data_$PID.csv"
+FJSON="data_$PID.json"
+FWEIGHTS="weights_$PID.npy"
+FRESULT="results_$PID"
 CONDA_BASE=$(conda info --base)
+
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate wext
-python process_mutations.py -m data.csv -ct NA -o data.json
-python compute_mutation_probabilities.py -mf data.json -np 1000 -nc 6 -wf weights.npy -v 1
-python find_sets.py -mf data.json -wf weights.npy -s exclusivity -k $1 -c 6 -o results
+python process_mutations.py -m $FDATA -ct NA -o $FJSON
+python compute_mutation_probabilities.py -mf $FJSON -np 1000 -nc 1 -wf $FWEIGHTS -v 1
+python find_sets.py -mf $FJSON -wf $FWEIGHTS -s exclusivity -k $1 -c 1 -o $FRESULT
 conda deactivate
+rm $FDATA $FJSON $FWEIGHTS
